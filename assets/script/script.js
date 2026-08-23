38f1085ee21c0c4ac06c7e98995ddb1362548052
imanamiri-v <imanamiri-iran-lover@gmail.com>
2026-08-23 18:41:54 +0330

    doing some of design work & handling add btn

---
+++ assets/script/script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("Form");
+const firstName = document.getElementById("nameInput");
+const lastName = document.getElementById("lastNameInput");
+const mail = document.getElementById("mailInput");
+const job = document.getElementById("jobInput");
+const phoneNumber = document.getElementById("phoneNumberInput");
+const gender = document.getElementById("genderInput");
+
+let users = [];
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const newPerson = {
+    firstName : firstName.value.trim(),
+    lastName : lastName.value.trim(),
+    mail : mail.value.trim(),
+    job : job.value.trim(),
+    phoneNumber : phoneNumber.value.trim(),
+    gender : gender.value
+  }
+  users.push(newPerson)
+  form.reset();
+})
