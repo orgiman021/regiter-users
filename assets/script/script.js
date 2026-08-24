dbf732f39dffde89500b2838864825659a1155cf
imanamiri-v <imanamiri-iran-lover@gmail.com>
2026-08-24 15:51:27 +0330

    complete the form statment with conditions

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,20 +5,27 @@ const mail = document.getElementById("mailInput");
 const job = document.getElementById("jobInput");
 const phoneNumber = document.getElementById("phoneNumberInput");
 const gender = document.getElementById("genderInput");
-const overlay = document.getElementById('overlay')
-const modal = document.getElementById('modal')
+const overlay = document.getElementById("overlay");
+const modal = document.getElementById("modal");
+const errorMsg = document.getElementById("errorMsg");
 let users = [];
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const newPerson = {
-    firstName : firstName.value.trim(),
-    lastName : lastName.value.trim(),
-    mail : mail.value.trim(),
-    job : job.value.trim(),
-    phoneNumber : phoneNumber.value.trim(),
-    gender : gender.value
+    firstName: firstName.value.trim(),
+    lastName: lastName.value.trim(),
+    mail: mail.value.trim(),
+    job: job.value.trim(),
+    phoneNumber: phoneNumber.value.trim(),
+    gender: gender.value,
+  };
+  if (!firstName.value || !lastName.value || !mail.value) {
+    errorMsg.innerHTML = "لطفا نام و نام خانوادگی و ایمیل را وارد نمایید";
+    return;
+  } else {
+    errorMsg.innerHTML = "";
+    users.push(newPerson);
+    form.reset();
   }
-  users.push(newPerson)
-  form.reset();
-})
+});
